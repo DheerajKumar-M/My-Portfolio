dbee1e0f2100e95af685de4883047e6925f46dbd
DheerajKumar-M <maddinenidheeraj14@gmail.com>
2025-01-29 13:31:47 -0600

    Adding Skills Section

---
+++ src/Container/skills/utils.js
@@ -0,0 +1,69 @@
+export const skillsData=[
+    {
+        label: "FRONT END",
+        data:[
+            {
+                skillName: "HTML",
+                percentage: "90",
+            },
+            {
+                skillName:"CSS",
+                percentage:"50"        
+            },
+            {
+                skillName:"JavaScript",
+                percentage:"70"        
+            },
+            {
+                skillName:"Bootstrap",
+                percentage:"80"
+            },
+            {
+                skillName:"ReactJS",
+                percentage:"80"
+            }
+        ]
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "Java",
+                percentage: "80",
+            },
+            {
+                skillName: "Python",
+                percentage: "50"
+            },
+            {
+                skillName: "C",
+                percentage: "70"
+            },
+            {
+                skillName: "C++",
+                percentage: "70"
+            },
+            {
+                skillName:"Spring Framework",
+                percentage:"80"
+            }
+        ]
+    },
+    {
+        label: "DATABASE",
+        data: [
+            {
+                skillName:"MySQL",
+                percentage:"80"
+            },
+            {
+                skillName:"MongoDB",
+                percentage:"70"
+            },
+            {
+                skillName:"PostgreSQL",
+                percentage:"60"
+            }
+        ]
+    }
+]
